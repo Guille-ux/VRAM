f61646e43f4d11e571adfb680da71a0d26483bda
Guille <guilleleiratemes@gmail.com>
2025-01-08 18:15:13 +0000

    the repository is finished, but in the future i'am going to add a system for arrays and other structures

--- vram.h
+++ vram.h
@@ -6,7 +6,7 @@ typedef struct {
 	char free = 1;
 } Var;
 
-Var virtual_ram[RAM_LEN]; 
+Var virtual_ram[RAM_LEN];
 
 void ram_init() {
 	for (int i = 0; i < RAM_LEN; i++) {
@@ -21,19 +21,42 @@ int new_var(const char *name, char data) {
 	}
 	for (int i = 0; i < RAM_LEN; i++) {
 		if (virtual_ram[i].free) {
-			return 1;
 			virtual_ram[i].free = 0;
 			for (int f = 0; f < 6; f++) {
 				virtual_ram[i].name[f] = name[f];
 			}
+			return 1;
 		}
 	}
 }
-int search_var(const char *name) { // ALL VARS NEED TO FINNISH IN '\n'
+int search_var(const char *name) { // ALL VARS NEED TO FINNISH IN FIN_SYMBOL AT LIBS.H
 	for (int i = 0; i < RAM_LEN; i++) {
 		if (mystrcmp(virtual_ram[i].name, name)) {
-			return i;
+			if (virtual_ram[i].free) {
+				return i;
+			}
 		}
+	}
+	return -1;
+}
+void delete_var(const char *name) {
+	int pos = search_var(name);
+	if (pos > 0) {
+		virtual_ram[pos].free = 1;
+	}
+}
+
+void write_var(const char *name, char data) {
+	int pos = search_var(name);
+	if (pos > 0) {
+		virtual_ram[pos].data = data;
+	}
+}
+
+char read var(const char *name) {
+	int pos = search_var(name);
+	if (pos > 0) {
+		return virtual_ram[pos].data;
 	}
 	return -1;
 }
