35abad5cbab565fe1ffcf13db46d44fbf8144698
Guille-ux <104579306+Guille-ux@users.noreply.github.com>
2025-01-18 15:03:54 +0000

    Update lib.h

--- libs/lib.h
+++ libs/lib.h
@@ -1,6 +1,6 @@
 #define FIN_SYMBOL '_'
 
-int len(const char *string) {
+int vram_len(const char *string) {
     int length = 0;
     unsigned char ch;
     while ((ch = string[length]) != FIN_SYMBOL) {
@@ -9,11 +9,11 @@ int len(const char *string) {
 
     return length;
 }
-int mystrcmp(const char *str1, const char *str2) {
-	if (len(str1) != len(str2)) {
+int vram_strcmp(const char *str1, const char *str2) {
+	if (vram_len(str1) != vram_len(str2)) {
 		return 0;
 	} else {
-		for (int i = 0; i < len(str1); i++) {
+		for (int i = 0; i < vram_len(str1); i++) {
 			if (str1[i] != str2[i]) {
 				return 0;
 			}
