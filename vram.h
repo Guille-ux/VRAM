7aa5845a1bc6ed9e0c8d05632d80bb3a29f1f52f
Guille <guilleleiratemes@gmail.com>
2025-03-29 11:33:20 +0000

    anything

--- vram.h
+++ vram.h
@@ -1,12 +1,25 @@
+/* This program is free software: you can redistribute it and/or modify
+/* it under the terms of the GNU General Public License as published by
+/* the Free Software Foundation, either version 3 of the License, or
+/* (at your option) any later version.
+/* This program is distributed in the hope that it will be useful,
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+/* GNU General Public License for more details.
+/* You should have received a copy of the GNU General Public License
+/* along with this program. If not, see <https://www.gnu.org/licenses/>.
+/* Copyright (c) 2025 Guillermo Leira Temes
+/* */
+
 #ifndef VRAM_H
 #define VRAM_H
 
-#define RAM_LEN 134217728
+#define RAM_LEN 1000 // changing this 134217728 for this 1'000
 
 #include "libs/lib.h"
 
 typedef struct {
-	const char name[6];
+	char name[7];
 	char data;
 	char free;
 } Var;
@@ -22,7 +35,7 @@ void ram_init() {
 int search_var(const char *name) { // ALL VARS NEED TO FINNISH IN FIN_SYMBOL AT LIBS.H
 	for (int i = 0; i < RAM_LEN; i++) {
 		if (vram_strcmp(virtual_ram[i].name, name)) {
-			if (virtual_ram[i].free) {
+			if (virtual_ram[i].free==0) {
 				return i;
 			}
 		}
@@ -38,23 +51,24 @@ int new_var(const char *name, char data) {
 	for (int i = 0; i < RAM_LEN; i++) {
 		if (virtual_ram[i].free) {
 			virtual_ram[i].free = 0;
-			for (int f = 0; f < 6; f++) {
+			for (int f = 0; f < 7; f++) {
 				virtual_ram[i].name[f] = name[f];
 			}
+			virtual_ram[i].data = data;
 			return 1;
 		}
 	}
 }
 void delete_var(const char *name) {
 	int pos = search_var(name);
-	if (pos > 0) {
+	if (pos >= 0) {
 		virtual_ram[pos].free = 1;
 	}
 }
 
 void write_var(const char *name, char data) {
 	int pos = search_var(name);
-	if (pos > 0) {
+	if (pos >= 0) {
 		virtual_ram[pos].data = data;
 	}
 }
